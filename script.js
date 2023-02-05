e65320879d76cded52bdec81b62a505ff1a1b09e
Helitha Rupasinghe <hr21don>
2023-02-05 22:06:52 +0000

    first commit 👨‍🚀

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get the button
+var scrollToTopBtn = document.getElementById("btn-top");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
